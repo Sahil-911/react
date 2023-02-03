fc83f5944280c8cd9a326c0916be768726a49c32
Sahil <Sahillakdawala1109@gmail.com>
2023-02-03 19:32:24 +0530

    installing tailwind

--- jarvis/src/App.js
+++ jarvis/src/App.js
@@ -1,21 +1,21 @@
 // import logo from './logo.svg';
-import './App.css';
+import './index.css';
 import Employee from './components/Employee.js';
 
 function App() {
   console.log('we are going to show the list of the employee');
   const ShowEmployees= true;
   return (
-    <div className="App">
+    <div className="h-14 bg-gradient-to-r from-sky-300 to-indigo-300">
     <p>
       Hello, how are you!!
     </p>
     {ShowEmployees ?
-      <>
-        <Employee />
-        <Employee />
-        <Employee />   
-      </>
+      <p3 className="bg-red-300">
+        <Employee name='anuj' role='Timepass'/>
+        <Employee name='chacha'/>
+        <Employee name='harshal'/>   
+      </p3>
       :<p>No, you can't see the Employee </p>
     }
   </div>
--- jarvis/src/components/Employee.js
+++ jarvis/src/components/Employee.js
@@ -1,5 +1,13 @@
-function Employee(){
-    return <h3> the employee is here...And Anuj is also.</h3>;
+function Employee(props){
+    return (
+        <>
+        < input  type = 'text' onChange = {(q) => 
+            console.log(q.target.value)        }></input>
+        <h3> the employee is here...And {props.name} is also.</h3>
+        {/* <p>{props.role ? 'role: ' + props.role : 'No role'}</p> */}
+        {props.role ? <p>{props.role}</p> : <p>No role</p>}
+        </>
+    )
 }
 
 export default Employee;
